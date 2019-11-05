480ca7cb4daf074e0631e5e53d300849a4e64b3e
HamzaBenmessaoud <48830747+HamzaBenmessaoud@users.noreply.github.com>
2019-11-05 16:56:16 +0100

    ajout update pour book

--- NodeJs - MongoDB/src/routes/book.js
+++ NodeJs - MongoDB/src/routes/book.js
@@ -4,7 +4,7 @@ const Books = require('../controllers/booksControllers'),
 
 route.get('/books', Books.listBooks)
 route.get('/book/:title', Books.listBooks)
-
+route.post('/book/:id', Books.updateBook)
 route.post('/book', Books.saveBook)
 
 module.exports = route
